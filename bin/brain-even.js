f1b63996ddd46662febc5fc3e8896b0bd7642ad5
Irina <irinanilova0402@gmail.com>
2022-10-25 10:34:49 +0300

    brain-calc.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,29 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import brainEven from '../games/even-game.js';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + name + '!');
+brainEven();
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-for (let i = 0; i < 3; i += 1) {
-  let numberIsRandom = Math.floor(Math.random() * 100) + 1;
-  let rightAnswer = '';
-  if (numberIsRandom % 2 === 0) {
-    rightAnswer = 'yes';
-  } else if (numberIsRandom % 2 !== 0) {
-    rightAnswer = 'no';
-  }
-  console.log('Question: ' + numberIsRandom);
-  const userAnswer = readlineSync.question('Your answer: ');
-  if (userAnswer === rightAnswer) {
-    console.log('Correct!');
-    if (i === 2) {
-      console.log(`Congratulations, ${name}!`);
-    }
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.
-    Let's try again, ${name}!`);
-    i = 3;
-  }
-}
+
